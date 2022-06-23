ca67013a0f0b196a8feb0eb2bcf821db0df480cc
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2022-06-24 00:01:33 +0200

    remove .env from git

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 )
 
 func main() {
+	//load .env if it exists
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+	log.Info("SideCar logger injector worker started! \n")
 	watcher := loggerInjector.NewWatcher()
-
+	//start watch k8s
 	watcher.Watch(context.Background())
 }
